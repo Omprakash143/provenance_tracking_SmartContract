3cda9a1e7b79c53a150de8d8f20230ab77b78724
Omprakash143 <prakash1431125669@gmail.com>
2019-04-06 22:40:06 +0530

    Add files via upload

---
+++ provenance_tracking_smartContract/README.txt
@@ -0,0 +1,23 @@
+Smart contract name is : TrackingSystem.
+
+1. The smart contract can track the assets from source(manufacturer) to destination(consumer) throught which intermidiaries the product passes.
+2. Smart contract can track the product ownership.
+3. It can track the position of the product i.e, right now where the product is using productAt(refer code).
+4. It can make sure that the product is reached to next stages in given time and can display a message if the product gets delayed at any stage and the amount of time the product delayed.
+5. Allow only right owner at right position to call the updateStages() method to update the stage(productAt).
+6. Make sure status is updated as per given owner(address) order i.e, pos2 owner cannot update the stage(productAt) before reaching the product to pos1 owner.
+
+
+TEST CASES:
+	1. Check whether the product received in given time. if not, possibility that the product may damage.
+
+	REASON: Assume if the product is vegetables. If they get delayed at intermediaries there may chance of damage. So the consumer can undo purchasing them.
+
+	2. Check whether the stages are updated in provided correct order.
+	REASON: If not, the overall aim of this contract is useless. chances that some may pretend this product owner is X but they bought the product from Y.
+
+	3. Provide product delay time and message.
+	REASON: As time is important, upto how much time the product is safe can be dicidedby the consumer before buying the product.
+
+	4. Only intermediary owner's allowed to invoke the updateStage() method.
+	REASON: To make sure the integrity of the product i.e, to make sure that the product is not modified and to find any fault easily.
---
+++ provenance_tracking_smartContract/tracking.sol
@@ -0,0 +1,191 @@
+pragma solidity ^0.4.25;
+
+
+
+/*	test cases 1. check whether the product received in given time. if not possibility that product may damage.
+
+test case 2. check whether the stages are updated in provided correct order.
+
+test case 3. provide product delay time and message.
+		*/
+
+contract TrackingSystem{
+
+
+	// stages(intermediaries) the product pass through.
+	
+	enum productStages { source , pos1 , pos2 , pos3 , consume }
+
+	// variable to refer productStages.
+ 		
+productStages public productAt;	
+							
+
+	
+	uint public currentTime;
+
+
+	
+	string public message;
+
+
+	
+	uint public delay;
+
+
+
+	// store the address of owner who create the instane of smart contract.
+
+		address public owner; 	
+
+	// mapping to check the correct owner at the stage the product passing through.
+		mapping( bytes32 => address ) stageOwner; 	 
+
+     	//constructor , an instance of TrackingSystem smart contract called when contract instance is created.
+		constructor(address ad1 , address ad2 , address ad3){	
+		
+		// contract creator is the owner of the product.
+			owner = msg.sender;   
+
+ 		// store the current time.					
+	
+		currentTime = now;
+
+
+		// store that product is verified by owner and moving towards pos1, store the product source stage.
+			productAt = productStages.source;
+
+		// storing owner address so that he can't update the stage(productAt) status at other stages.									
+			stageOwner[sha3(productStages.source)] = msg.sender;
+
+
+		// stages(intermediaries)owner addresses from where the product pass through.
+			stageOwner[sha3(productStages.pos1)] = ad1;
+
+			stageOwner[sha3(productStages.pos2)] = ad2;
+
+			stageOwner[sha3(productStages.pos3)] = ad3;
+
+		  
+		}// end of constructor.
+
+
+
+
+
+/*
+	Below shows the stage and respective stage owner only who can call the contract to update the stage variable.
+*/
+
+ 
+
+
+// function to update product stages.
+
+
+
+	function updateProductStage() public{
+
+
+
+	//check product is moved from source to pos1.
+		if(productAt==productStages.source){
+
+		// if moved , check whether the pos1 owner is updating the status or not.
+			if(msg.sender==stageOwner[sha3(productStages.pos1)]){
+
+
+			// check whether the product reached pos1 from owner in specified time say: 10 seconds here (useful if time get delayed product may damage).
+			// if delayed.
+			    if(now > currentTime+10 seconds){
+
+			        delay = now - currentTime +10 seconds;
+                              	message = "product delayed to pos1, please check the delay ";
+       			        productAt = productStages.pos1;
+                            }else{
+			// if not delayed.
+			        delay = 0;
+
+				productAt = productStages.pos1;
+
+			    }
+
+			    currentTime = now;
+
+			}
+
+		}
+
+
+	//check product is moved from pos1 to pos2.
+		if(productAt==productStages.pos1){
+
+
+		// if moved , check whether the pos2 owner is updating the status or not.
+			if(msg.sender==stageOwner[sha3(productStages.pos2)]){
+
+
+			// check whether the product reached pos2 from pos1 in specified time say: 10 seconds here (useful if time get delayed product may damage).
+			// if delayed.
+			    if(now > currentTime+10 seconds){
+
+			        delay = now - currentTime +10 seconds;
+
+			        message = "product delayed to pos2, please check the delay ";
+
+			        productAt = productStages.pos2;
+
+			    }else{
+
+			// if not delayed.
+			        delay = 0;
+
+				productAt = productStages.pos2;
+
+			    }
+
+			    currentTime = now;
+
+			}
+
+		}
+	
+
+	//check product is moved from pos2 to pos3.	
+		if(productAt==productStages.pos2){
+
+
+		// if moved , check whether the pos3 owner is updating the status or not.
+			if(msg.sender==stageOwner[sha3(productStages.pos3)]){
+
+
+			// check whether the product reached pos3 from pos2 in specified time say: 10 seconds here (useful if time get delayed product may damage).
+			    if(now > currentTime+10 seconds){
+
+			        delay = now - currentTime +10 seconds;
+
+			        message = "product delayed to pos3, please check the delay ";
+
+			        productAt = productStages.pos3;
+
+			    }else{
+			// if not delayed.
+			        delay = 0;
+
+				productAt = productStages.pos3;
+
+			    }
+
+			    currentTime = now;
+
+			}
+
+		}
+
+	}// end of updateProductStage method.
+
+
+
+
+}
+// end of contract.
